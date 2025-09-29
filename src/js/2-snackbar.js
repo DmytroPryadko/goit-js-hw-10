e1763bae2afa699cf17c4a7688321316bdae9107
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2025-09-29 18:52:11 +0200

    11

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,62 +1,44 @@
+// Описаний у документації
 import iziToast from "izitoast";
+// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector(".form");
 
-form.addEventListener("submit", checkSubmit)
 
-function checkSubmit(evt) {
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
     evt.preventDefault();
-    const delay = evt.target.elements.delay;
-    const state = evt.target.elements.state;
-    const enteredNum = delay.value;
-    const choice = state.value;
+    
 
-    makePromise(enteredNum, choice)
-        .then(value => {
-            console.log(value);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    form.reset();
-}
+    const delay = parseInt(evt.target.delay.value);
+    const radio = evt.target.state.value;
 
-function makePromise(enteredNum, choice) {
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (choice === "fulfilled") {
-                resolve(iziToast.show({
-                    ...optionsFulfilled,
-                    message: `Fulfilled promise in ${enteredNum}ms`,
-                }))
-            } else {
-                reject(iziToast.show({
-                    ...optionsRejected,
-                    message: `Rejected promise in ${enteredNum}ms`,
-                }))
+            if (radio === "fulfilled") {
+                resolve(delay);
+            } else if (radio === "rejected") {
+                reject(delay);
             }
-        }, enteredNum);
+        }, delay);
     });
-};
 
-const optionsFulfilled = {
-    title: '✔',
-    titleColor: 'rgba(134, 253, 7, 1)',
-    titleSize: '24px',
-    messageColor: '#FFFFFF',
-    messageSize: '16px',
-    backgroundColor: 'rgba(89, 161, 13, 1)',
-    timeout: 4000,
-    position: 'topCenter',
-};
-const optionsRejected = {
-    title: '✖',
-    titleColor: 'rgba(255, 190, 190, 1)',
-    titleSize: '24px',
-    messageColor: '#FFFFFF',
-    messageSize: '16px',
-    backgroundColor: 'rgba(239, 64, 64, 1)',
-    timeout: 4000,
-    position: 'topCenter',
-};
+    promise
+        .then((delay) => {
+            iziToast.success({
+                
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch ((delay) => {
+        iziToast.error({
+            
+            message: `❌ Rejected promise in ${delay}ms`,
+        });
+    })
+    
+}
